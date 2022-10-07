a51077f5d2dc5c0708e9aa9630b925e4ad7a616f
sameoldstories <metalzero4@gmail.com>
2022-10-07 20:35:58 +0600

    Добавьте файлы проекта.

---
+++ Abstraction.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+using namespace std;
+
+class Smartphone {
+public:
+    virtual void TakeASelfie()=0;
+    virtual void MakeACall() = 0;
+
+};
+class Android:public Smartphone {
+public:
+    void TakeASelfie() {
+        cout << "Android selfie\n";
+    }
+    void MakeACall() {
+        cout << "Android calling\n";
+    }
+};
+class IPhone :public Smartphone {
+public:
+    void TakeASelfie() {
+        cout << "iPhone selfie\n";
+    }
+    void MakeACall() {
+        cout << "iPhone calling\n";
+    }
+};
+
+int main()
+{
+    Smartphone* s1 = new Android();
+    s1->TakeASelfie();
+    s1->MakeACall();
+    Smartphone* s2 = new IPhone();
+    s2->TakeASelfie();
+    s2->MakeACall();
+
+
+
+
+    system("pause>0");
+}
